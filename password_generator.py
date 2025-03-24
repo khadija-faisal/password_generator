451e1aeae04f80dd44d0380dca8d38cd5e184e14
Khadija_Faisal <160174292+khadija-faisal@users.noreply.github.com>
2025-03-25 01:57:10 +0500

    password strength meter

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,61 @@
-import streamlit as st
+import streamlit as st # type: ignore
 import random
 import string
+import re 
+# password strength checker
 
+st.set_page_config(page_title="Password Strength Checker and Password Generator", page_icon="🔒")
+
+st.title("Password Strength Checker🔐")
+st.markdown(""" 
+ welcome to the ultimate Password Strength Checker and Password Generator!
+            
+ use this simple tool to check the strength of your password and generate a strong password.""")
+
+
+password = st.text_input("Enter your password", type="password")
+
+feedback = []
+
+score = 0
+
+if password:
+    if len(password) >= 8:
+        score += 1
+    else:
+        feedback.append("❌Password should be at least 8 characters long")
+    
+    if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
+        score += 1
+    else:
+        feedback.append("❌Password should contain both uppercase and lowercase characters")
+    
+    if re.search(r"\d", password):
+        score += 1
+    else:
+        feedback.append("❌Password should contain at least one number")
+    if re.search(r"[!@#$&^%*<>]", password):
+        score += 1
+    else:
+        feedback.append("❌Password should contain at least one special character (!@#$&^%*<>)")
+    if score == 4:
+        feedback.append("✅Password is strong.")
+    elif score == 3:
+        feedback.append("🟡Password is medium Strength, It could be stronger.")
+    else:
+        feedback.append("🔴Password is weak, Make it stronger.")
+    
+    if feedback:
+        st.markdown("## Improvement Suggestions")
+        for suggestion in feedback:
+            st.write(suggestion)
+
+else:
+    st.info("Enter a password to check its strength")
+
+st.write("____________________________________________________________________")
+
+# Password Generator
 def password_generator(length, use_digits, use_special_characters):
     characters = string.ascii_letters
     if use_digits:
